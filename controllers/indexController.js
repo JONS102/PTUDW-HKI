d6734e28eed6c9df77d1d5a8afc5b0469f9a8be0
JONS102 <nguyenchinhngoc1001@gmail.com>
2025-05-24 08:12:23 +0700

    câu 4,câu 5

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,2 +1,36 @@
-const models = require('../models/index');
-models.sequelize.sync().then(() => console.log('Table created successfully!'));
+const controller = {};
+const models = require("../models");
+
+controller.show = async(req, res) => {
+    let limit = 6;
+    //page có thể truyền trên query string
+    let page = isNaN(req.query.page) ? 1 : parseInt(req.query.page);
+
+    let foods = await models.Food.findAll({
+        attributes: ['name', 'imagePath', 'price', 'summary'],
+        order: [
+            ["price", "ASC"]
+        ],
+        // limit: 6,// có phân trang nên có thể bỏ limit
+    });
+    let totalRows = foods.length;
+    let offset = (page - 1) * limit;
+    foods = foods.slice(offset, offset + limit);
+    foods.forEach((item, index) => {
+        item.price = parseFloat(item.price).toFixed(2);
+        item.isRight = index == 2 || index == 3;
+    });
+    res.locals.foods = foods;
+    res.locals.pagination = {
+        page,
+        limit,
+        totalRows,
+    }
+    res.render("index");
+};
+
+controller.reserve = (req, res) => {
+    res.render("index");
+};
+
+module.exports = controller;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const expressHbs = require('express-handlebars');
+const { createPagination } = require('express-handlebars-paginate');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -10,12 +11,26 @@ app.engine("hbs",
         layoutsDir: __dirname + "/views/layouts",
         defaultLayout: "main",
         extname: ".hbs",
+        runtimeOptions: {
+            allowProtoPropertiesByDefault: true,
+        },
+        helpers: {
+            createPagination,
+        },
     })
 );
 app.set("view engine", "hbs");
 // app.get('/', (req, res) => {
 //     res.send('Hello World!');
 // });
+
+//câu 3=> /=> GET
+
+//câu 4=> /?page=1 =>GET
+
+//câu 5 => /=> POST 
+app.use("/", require("./routes/indexRouter"));
+
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost: ${port}`);
